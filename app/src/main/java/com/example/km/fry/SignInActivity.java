11b1884e99bd29d705f2dfdbe79a33cdd47d0adb
KM <sodp2@naver.com>
2018-07-29 01:54:51 +0900

    스플래시 액티비티 생성

---
+++ app/src/main/java/com/example/km/fry/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.example.km.fry;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.km.fry.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
---
+++ app/src/main/java/com/example/km/fry/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.km.fry;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView splashImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        splashImage = (ImageView) findViewById(R.id.egg);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashImage.setImageResource(R.drawable.splash_2);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                        startActivity(intent);
+                    }
+                }, 1000);
+            }
+        }, 1000);
+    }
+}
