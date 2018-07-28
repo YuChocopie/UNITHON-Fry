912504b04abc89b49c6c7a41539454a7028d82d1
KM <sodp2@naver.com>
2018-07-28 17:47:16 +0900

    퍼미션 체크 순서 문제 처리

--- app/src/main/java/com/example/km/fry/MainActivity.java
+++ app/src/main/java/com/example/km/fry/MainActivity.java
@@ -33,77 +33,6 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
         }
-
-        location.requestSingleUpdate(this.getApplicationContext(),
-                new location.LocationCallback() {
-                    @Override
-                    public void onNewLocationAvailable(location.GPSCoordinates location) {
-
-                        float lat = location.latitude;
-                        float lng = location.longitude;
-
-                        Log.d("lat: ", lat + "");
-                        Log.d("lng: ", lng + "");
-
-                        // default
-                        if (lat == 0.0f && lng == 0.0f)
-                        {
-                            lat = 37.54664f;
-                            lng = 126.94988f;
-                        }
-
-                        regionString = HomeActivity.getAddress(MainActivity.this, lat, lng);
-                        splitString();
-
-                        Intent intent = new Intent(getApplication(), HomeActivity.class);
-
-                        intent.putExtra("MyLat", lat);
-                        intent.putExtra("MyLng", lng);
-                        startActivity(intent);
-                    }
-                });
-
-        HashMap<String, Integer> regionMap;
-
-        String[] urls = new String[4];
-        String jsonText = ".json.txt";
-
-        // 지역 URL
-        urls[1] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt";
-        urls[2] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.";
-        urls[3] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.";
-
-        try {
-
-            // 현재 지역의 지역 코드를 검색
-            for (int i = 1; i <= urls.length; ++i)
-            {
-                URL url = new URL(urls[i]);
-
-                RegionAsyncTask regionTask = new RegionAsyncTask();
-
-                regionTask.execute(url);
-
-                regionMap = regionTask.get();
-
-                if (i == 3)
-                {
-                    // 동코드: region[3]
-                    Log.d("region code: ", regionMap.get(region[i]) + "");
-                    break;
-                }
-
-                urls[i + 1] += String.valueOf(regionMap.get(region[i]));
-                urls[i + 1] += jsonText;
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
     }
 
     public void splitString()
@@ -115,6 +44,89 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < region.length; ++i)
         {
             Log.d("length: ", region[i]);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == 0)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                location.requestSingleUpdate(this.getApplicationContext(),
+                        new location.LocationCallback() {
+                            @Override
+                            public void onNewLocationAvailable(location.GPSCoordinates location) {
+
+                                float lat = location.latitude;
+                                float lng = location.longitude;
+
+                                Log.d("lat: ", lat + "");
+                                Log.d("lng: ", lng + "");
+
+                                // default
+                                if (lat == 0.0f && lng == 0.0f)
+                                {
+                                    lat = 37.54664f;
+                                    lng = 126.94988f;
+                                }
+
+                                regionString = HomeActivity.getAddress(MainActivity.this, lat, lng);
+                                splitString();
+
+                                Intent intent = new Intent(getApplication(), HomeActivity.class);
+
+                                intent.putExtra("MyLat", lat);
+                                intent.putExtra("MyLng", lng);
+                                startActivity(intent);
+                            }
+                        });
+
+                HashMap<String, Integer> regionMap;
+
+                String[] urls = new String[4];
+                String jsonText = ".json.txt";
+
+                // 지역 URL
+                urls[1] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/top.json.txt";
+                urls[2] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/mdl.";
+                urls[3] = "http://www.kma.go.kr/DFSROOT/POINT/DATA/leaf.";
+
+                try {
+
+                    // 현재 지역의 지역 코드를 검색
+                    for (int i = 1; i <= urls.length; ++i)
+                    {
+                        URL url = new URL(urls[i]);
+
+                        RegionAsyncTask regionTask = new RegionAsyncTask();
+
+                        regionTask.execute(url);
+
+                        regionMap = regionTask.get();
+
+                        if (i == 3)
+                        {
+                            // 동코드: region[3]
+                            Log.d("region code: ", regionMap.get(region[i]) + "");
+                            break;
+                        }
+
+                        urls[i + 1] += String.valueOf(regionMap.get(region[i]));
+                        urls[i + 1] += jsonText;
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                Log.d("check: ", "check permission");
+            }
         }
     }
 }
