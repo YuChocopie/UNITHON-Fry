f112d7c5368ced924eb6b416f44fcae6baa3326a
KM <sodp2@naver.com>
2018-07-29 03:42:23 +0900

    SignIn 액티비티 생성

--- app/src/main/java/com/example/km/fry/SignInActivity.java
+++ app/src/main/java/com/example/km/fry/SignInActivity.java
@@ -6,24 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class SignUpActivity extends AppCompatActivity{
+public class SignInActivity extends AppCompatActivity {
 
     ImageButton config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
 
-        config = (ImageButton) findViewById(R.id.up_config);
+        config = (ImageButton) findViewById(R.id.in_config);
 
         config.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent loginIntent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
+                Intent mainIntent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(mainIntent);
                 finish();
             }
         });
+
     }
 }
